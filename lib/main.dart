4ec64df1d8ec64e3b4e6daad65bb62fddd99e3eb
Your Name <you@example.com>
2026-08-07 12:03:14 +0530

    firstcomit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NetFlix());
+}
+class NetFlix extends StatelessWidget {
+  const NetFlix({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
